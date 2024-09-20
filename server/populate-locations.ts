e0c1c079964aa360095f26a0d4101a0f5d4f7c83
3dyuval <3dyuval@gmail.com>
2024-09-20 23:47:11 +0300

    populate locations

--- server/populate-locations.ts
+++ server/populate-locations.ts
@@ -1,13 +1,16 @@
-import dotenv from 'dotenv'
-import axios, { Axios } from 'axios'
+import { config } from 'dotenv'
+import axios from 'axios'
+import { MySQL } from './mysql.storage.ts'
+import { DonationLocationDate } from '../src/types'
 
-dotenv.config()
+
+config()
 
 if (!process.env.WP_API_URL || !process.env.WP_USERNAME || !process.env.WP_PASSWORD) {
   throw new Error('WP_API_URL, WP_USERNAME, or WP_PASSWORD environment variable is missing.')
 }
 
-const config = {
+const wp = axios.create({
   baseURL: process.env.WP_API_URL,
   auth: {
     username: process.env.WP_USERNAME,
@@ -15,115 +18,87 @@ const config = {
   },
   headers: {
     'Content-Type': 'application/json',
-    'Accept': '*/*'
+    'Accept': 'application/json',
   },
   timeout: 10_000,
-}
-
-const wp = new Axios(config)
+  family: 4,
+})
 
 async function populateWpData() {
 
-  await wp.options(`/locations`)
-    .then(res => console.log(res.data))
+
+  await wp.get('/users', {
+    params: {
+      context: 'edit',
+    },
+  })
+    .then(res => console.log('Succesfully accesed wp-json/wp/v2/users'))
     .catch(console.error)
 
-  // await wp.get('/users', {
-  //   params: {
-  //     context: 'edit',
-  //   },
-  // })
-  //   .then(res => console.log(res.data))
-  //   .catch(console.error)
+
+  const db = await MySQL.getInstance().init()
+
+  const data: DonationLocationDate[] = await db.getLocations()
+
+  console.log(data.length + ' locations found in database')
+
+  function decodeTitle(response: { data: [{ title: { rendered: string } }] }) {
+    return response.data.map(item => decodeURIComponent(item.title.rendered))
+  }
 
 
-  return process.exit(0)
+  const [wpLocationsTitles, wpSchedulesTitles,] = await Promise.all(
+    [
+      wp.get(`/locations`).then(decodeTitle),
+      wp.get(`/schedule`).then(decodeTitle),
+    ])
 
-  //
-  // // Initialize MySQL connection
-  // const db = await MySQL.getInstance().init()
-  //
-  // // Fetch the data
-  // const data: DonationLocationDate[] = await db.getLocations()
-  //
-  // console.log(data.length + ' locations found in database')
-  //
-  //
-  // for (const item of data) {
-  //   try {
-  //     const { dateOpen, dateClose, donationLocation } = item
-  //     const { name, schedulingUrl, address } = donationLocation
-  //     const { city, street, number } = address
-  //
-  //     // Prepare data for WordPress Locations
-  //     const postDataLocation = {
-  //       title: name,
-  //       content: `Address: ${street}, ${number}, ${city}`,
-  //       status: 'publish',
-  //       acf: {
-  //         city: city,
-  //         street: street,
-  //         number: number,
-  //       },
-  //     }
-  //
-  //     // Prepare data for WordPress Schedules
-  //     const postDataSchedule = {
-  //       title: `Schedule for ${name}`,
-  //       content: `Scheduling URL: ${schedulingUrl}`,
-  //       status: 'publish',
-  //       acf: {
-  //         from: dateOpen,
-  //         to: dateClose,
-  //       },
-  //     }
-  //
-  //     // Check if location already exists in WordPress
-  //     const searchResponse = await axios.get(`${wpApiUrl}/wp-json/wp/v2/locations`, {
-  //       params: {
-  //         search: name,  // Adjust this based on your search implementation
-  //       },
-  //     }).then(res => console.log(res.data))
-  //
-  //
-  //     const existingLocations = searchResponse.data
-  //     let existingLocation = existingLocations.find((loc: any) =>
-  //       loc.meta.acf_city === city &&
-  //       loc.meta.acf_street === street &&
-  //       loc.meta.acf_number === number,
-  //     )
-  //
-  //     if (existingLocation) {
-  //       console.log(`Location '${name}' already exists. Skipping.`)
-  //       continue
-  //     }
-  //
-  //     // Post to WordPress REST API for Locations
-  //     await axios.post(`${wpApiUrl}/wp-json/wp/v2/locations`, postDataLocation, {
-  //       headers: {
-  //         'Authorization': `Basic ${auth}`,
-  //         'Content-Type': 'application/json',
-  //       },
-  //     })
-  //
-  //     console.log(`Inserted location '${name}' into WordPress.`)
-  //
-  //     // Post to WordPress REST API for Schedules
-  //     await axios.post(`${wpApiUrl}/wp-json/wp/v2/schedules`, postDataSchedule, {
-  //       headers: {
-  //         'Authorization': `Basic ${auth}`,
-  //         'Content-Type': 'application/json',
-  //       },
-  //     })
-  //
-  //     console.log(`Inserted schedule for '${name}' into WordPress.`)
-  //
-  //   } catch (error) {
-  //     console.error('Error processing data:', error.response ? error.response.data : error.message)
-  //   }
-  // }
-  //
-  //console.log('Data successfully inserted into WordPress!')
+  console.log(wpLocationsTitles, wpSchedulesTitles)
+
+
+  const requests = new Set()
+
+  for (const item of data) {
+    const { dateOpen, dateClose, donationLocation } = item
+    const { name, schedulingUrl, address } = donationLocation
+    const { city, street, number } = address
+
+    if (!wpLocationsTitles.includes(name)) {
+      requests.add(wp.post(`/locations`, {
+        title: name,
+        content: `Address: ${street}, ${number}, ${city}`,
+        status: 'publish',
+        acf: {
+          city: city,
+          street: street,
+          number: number,
+        },
+      }).then(res => console.log(`Inserted locations for '${street}, ${number}, ${city}' into WordPress.`)))
+    }
+
+    if (!wpSchedulesTitles.some(title => title.includes(`Schedule for ${name}`))) {
+      requests.add(wp.post(`/schedule`, {
+        title: `Schedule for ${name}`,
+        content: `Scheduling URL: ${schedulingUrl}`,
+        status: 'publish',
+        acf: {
+          from: dateOpen,
+          to: dateClose,
+        },
+      }).catch(console.error)
+        .then(res => console.log(`Inserted schedule for '${name}' from ${dateOpen} to ${dateClose} into WordPress.`)))
+    }
+  }
+
+
+  await Promise.allSettled(requests)
+    .then(() => console.log('All requests finished'))
+
+  await db.connection.end()
+  console.log('Database connection closed')
+  process.exit(0)
 }
 
-populateWpData().catch(console.error)
+
+populateWpData()
+  .catch(console.error)
